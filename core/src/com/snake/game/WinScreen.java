76d925ab343a4e735f18ed17f4fe98869b5b45fa
toastmakerr <brianvu98@icloud.com>
2020-07-07 17:52:48 -0700

    Added randomized food spawns + win screen

---
+++ core/src/com/snake/game/WinScreen.java
@@ -0,0 +1,44 @@
+package com.snake.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+public class WinScreen extends ScreenAdapter {
+    SnakeGame game;
+    public WinScreen(SnakeGame game){
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
+
+            @Override
+            public boolean keyDown(int keyCode) {
+                if (keyCode == Input.Keys.SPACE) {
+                    game.setScreen(new TitleScreen(game));
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, .25f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(game.background,0,0);
+        game.batch.draw(game.winTitle,0,0);
+        game.batch.end();
+    }
+
+    @Override
+    public void hide(){
+        Gdx.input.setInputProcessor(null);
+    }
+
+}
