d5ba2f7e126e1fb627e7fd6c4c1fdf277d0ae8a3
toastmakerr <59041858+toastmakerr@users.noreply.github.com>
2020-07-14 01:38:53 -0700

    Deleted unused line

--- core/src/com/snake/game/model/GameObject.java
+++ core/src/com/snake/game/model/GameObject.java
@@ -3,7 +3,6 @@ package com.snake.game.model;
 import com.badlogic.gdx.math.Rectangle;
 
 public class GameObject extends Rectangle {
-    Rectangle newGameObj;
     public GameObject(float x, float y){
         this.set(x,y,40,40);
     }
